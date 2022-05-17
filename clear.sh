83a44b49753f014c3587eaca9a95f2e380564609
fraimparato <fra.imparato20@gmail.com>
2022-05-17 18:29:37 +0200

    Set up bibliography; updated .gitignore and clear.sh in order to clean up all the tmp useless files generated by the bibliography

--- clear.sh
+++ clear.sh
@@ -5,3 +5,7 @@ find . -name \*.log -type f -delete
 find . -name \*.out -type f -delete
 find . -name \*.synctex.gz -type f -delete
 find . -name \*.toc -type f -delete
+find . -name \*.run.xml -type f -delete
+find . -name \*.bbl -type f -delete
+find . -name \*.bcf -type f -delete
+find . -name \*.blg -type f -delete
